e920148da0695b3a5a72d86ce63e12ecce663df3
Shipusharma164cpm <sshivam1642000@gmail.com>
2026-06-18 14:41:35 +0530

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,6 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
 
     public static void main(String[] args) {
-        System.out.println("Login Test");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://demo.opencart.com/");
+
+        driver.findElement(By.name("search"))
+              .sendKeys("iPhone");
+
+        System.out.println("Search functionality executed successfully");
+
+        driver.quit();
     }
 }
